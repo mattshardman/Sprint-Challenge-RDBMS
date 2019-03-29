e3811625cc7459401593c6a6434ca5b061839fd9
mattshardman <mattshardman@gmail.com>
2019-03-29 09:23:26 +0000

    create server and route folders

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
-app.post('/api/projects', (req, res) => {
-    
-});
+app.use(helmet);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
